d53aae3124aa8c04f3bda41f8475cf41e9b6bf41
alegria.aitor <alegria.aitor@E110L36.opendeusto.es>
2019-09-24 08:44:38 +0200

    He cambiado el titulo de la ventana

--- prueba1/src/prueba1/ventana.java
+++ prueba1/src/prueba1/ventana.java
@@ -6,8 +6,8 @@ import javax.swing.JFrame;
 public class ventana extends JFrame{
 	public ventana(){
 		this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
-		this.setTitle("Ventana de Prueba");
-		this.setSize(300,100);
+		this.setTitle("He cambiado el titulo de la ventana");
+		this.setSize(640,408);
 		this.setVisible(true);
 	}
 
